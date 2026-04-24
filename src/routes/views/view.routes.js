5d5f6c5f856ebf2dedc5097732377906446c1d59
jefferson <jeffersonffers@hmail.com>
2026-04-24 13:59:55 +0200

    4567

--- src/routes/views/view.routes.js
+++ src/routes/views/view.routes.js
@@ -1,24 +1,14 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes.js'; 
 import dashboardRoutes from './dashboard.routes.js';
-// import repairRoutes from './repair.routes.js';
-import { isLoggedIn, requireAdmin, requireRole } from "../../middleware/auth.middleware.js";
+import clientRoutes from './client.routes.js';
+import { isLoggedIn } from "../../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.use('/', authRoutes)
-router.use('/dashboard', dashboardRoutes)
-//router.use('/dashboard',isLoggedIn, requireAdmin, dashboardRoutes)
+router.use('/dashboard',isLoggedIn , dashboardRoutes)
+router.use('/client',isLoggedIn , clientRoutes )
 //router.use('/repairs', repairRoutes)
-router.use('/client', clientRoutes )
-//router.use('/cars', carRoutes)
-
-router.get('/', (req, res) => {
-    res.render('index'); 
-});
-
-router.get('/servicios', (req, res) => {
-    res.render('service'); 
-});
 
 export default router;
